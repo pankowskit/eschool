fe7a466138bf378ec90ae69ff3d3df2e7312ef0a
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 21:14:01 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
+import { CategoriesModule } from './categories/categories.module';
 import { ComingsoonModule } from './comingsoon/comingsoon.module';
 import { ErrorModule } from './error/error.module';
+import { FooterComponent } from './footer/footer.component';
+import { HomeModule } from './home/home.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PlanModule } from './plan/plan.module';
 import { PrivacyModule } from './privacy/privacy.module';
 
 @NgModule({
@@ -18,7 +21,10 @@ import { PrivacyModule } from './privacy/privacy.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CategoriesModule,
     ComingsoonModule,
+    HomeModule,
+    PlanModule,
     PrivacyModule,
     ErrorModule
   ],
